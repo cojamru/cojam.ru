2088223f287218e6f6abd3a4f9599f3475fb5ad1
Cojam <e965@protonmail.com>
2017-10-10 21:06:56 +0500

    Дополненная инфа по трекам

--- source/js/db/music.js
+++ source/js/db/music.js
@@ -6,25 +6,50 @@ var musicDB = {
 			title: 'Quantum Suicide',
 			artist: 'Cojam',
 			feat: [ { nick: 'SHUMANGA', link: 'https://vk.com/public119295761' }, { nick: 'MrModez', link: 'https://soundcloud.com/mrmodez'} ],
-			description: '',
+			description: 'После событий "Despair Horizons" Крис обнаруживает себя мёртвой. Это конец истории? Или её смерть - начало чего-то большего?\n"Quantum Suicide" – это целый час кибер-мусора, вобравший в себя треки из разных компиляций, незавершённые синглы и прочий стафф, а часть музыки в этом альбоме записана в параллельных мирах. Этот альбом есть отражение мультивселенной – у треков здесь нет жанра или общей темы, и здесь вы услышите всё её разнообразие.',
 			release: '9 сентября 2017',
 			cover: { big: '/assets/img/music/quantum-suicide-cover.jpg', small: '' },
 			tracklist: [
-				{ title: 'Injoo' },
-				{ title: 'Lakaii' },
+				{
+					title: 'Injoo',
+					description: 'Из сборника Nintendocore Lives "Glitched" (2017).'
+				},
+				{
+					title: 'Lakaii',
+					description: 'Из сборника DeadDolphins & Cojam "Split it out!" (2017).'
+				},
 				{ title: 'Goodbye Sweet Life' },
-				{ title: 'Coal in My Sock' },
-				{ title: 'The Last Shot', feat: ['MrModez'] },
+				{
+					title: 'Coal in My Sock',
+					description: 'Из сборника A Grindcore Christmas "Volume 4" (2014).'
+				},
+				{
+					title: 'The Last Shot',
+					feat: ['MrModez'],
+					description: 'Из сборника "Cojam Secret CD" (2014).'
+				},
 				{ title: 'Loading Screen' },
 				{ title: 'SkyCrusher', feat: ['SHUMANGA'] },
 				{ title: 'Eat a Croissant' },
-				{ title: 'Apparatus Technology' },
+				{
+					title: 'Apparatus Technology',
+					description: 'Кавер на тему игры "Apparatus Technology"'
+				},
 				{ title: 'Super Space' },
-				{ title: 'The Confrontation' },
+				{
+					title: 'The Confrontation',
+					description: 'Первоапрельский трек для фанатского сообщества по игре "The Sims 2" в VK.'
+				},
 				{ title: 'White' },
-				{ title: 'Red Leaves' },
+				{
+					title: 'Red Leaves',
+					description: 'Потерянный трек из "グレイブブラッドの魔女" (2017).'
+				},
 				{ title: 'Death is Just a Beginning' },
-				{ title: 'Lessi Crip' },
+				{
+					title: 'Lessi Crip',
+					description: 'Трек из игры "Lessy Creep" (2015).'
+				},
 				{ title: 'Iotusatu' },
 				{ title: 'Outro' }
 			],
@@ -41,13 +66,38 @@ var musicDB = {
 			release: '29 июля 2017',
 			cover: { big: '/assets/img/music/despair-horizons-cover.jpg', small: '' },
 			tracklist: [
-				{ title: 'Kris\'s Dysphoria' },
-				{ title: 'Ashley Theme', artist: 'SHUMANGA' },
-				{ title: 'The Library' },
-				{ title: 'Datura', artist: 'SHUMANGA' },
-				{ title: 'Together' },
-				{ title: '"I Will Find Her"', artist: 'SHUMANGA' },
-				{ title: 'Good Ending', artist: 'SHUMANGA' }
+				{
+					title: 'Kris\'s Dysphoria',
+					description: 'Всё в жизни Крис шло наперекосяк. Она стала аутсайдером в школе и моменты, когда одноклассники не замечали её вовсе, были для ней лучом света. Это плачевно сказывалось на учёбе, что, в свою очередь, вылилось в скандалы в родительском доме. Последнее место, где её душа находит спокойствие - это тёмный лес и маленькая заброшенная библиотека в его глуби.'
+				},
+				{
+					title: 'Ashley Theme',
+					artist: 'SHUMANGA',
+					description: 'Прогуливаясь по лесу, Эшли искала в карманах джинсовки последнюю сигарету. Очередной прогул, очередной срыв на одноклассников. Она закурила. Голова гудела так, что даже сигаретный дым не мог помочь отвлечься от тяжелых мыслей. Задумчиво глядя под ноги, она не заметила, как перед ней появилось заброшенное здание. Безо всякого интереса, она открыла дверь. В большой комнате сидела Она.'
+				},
+				{
+					title: 'The Library',
+					description: 'Что скрывали страницы старых книг, собирающих пыль на полках этой библиотеки? То, что больше никогда не даст ей спокойно уснуть. Перед Крис открылся целый новый мир - изнанка реальности, сперва проявляющаяся в образе теней, поджидающих её у дома. Шумы и ощущение чьего-то присутствия стали поджидать Кристину на каждом углу. Об этой находке Крис спешила поделиться со своей новой подругой.'
+				},
+				{
+					title: 'Datura',
+					artist: 'SHUMANGA',
+					description: '..Увлеченная рассказами Крис, Эшли решила расслабиться. Конечно, ей слабо верилось в рассказы подруги. Она взяла свой порошок, рассыпала на столе...\nСпустя несколько минут она провалилась сквозь землю. Вокруг все сильнее проявлялось нечто сверхъестественное...тени, тусклое мерцание света, звуки, коих она никогда не слышала...Эшли поняла: "Все, что рассказывала Крис - сбывается, или же я схожу с ума..."'
+				},
+				{
+					title: 'Together',
+					description: 'Плохая успеваемость в школе в конце концов привела к жёсткому контролю со стороны родителей. Единственным способом видеться со своей подругой стала чёрная магия. Но прежде чем бросаться во все тяжкие, Крис стоило задуматься о последствиях выхода в потусторонний мир и о проклятии заброшенной библиотеки.'
+				},
+				{
+					title: '"I Will Find Her"',
+					artist: 'SHUMANGA',
+					description: '"Да кто они такие, чтобы отнимать её у меня? Почему все так сложно? День без веществ для меня ничто, по сравнению с днем без неё. Я обязательно найду и верну её...Крис, ты все сможешь, мы встретимся Там, несмотря ни на что."'
+				},
+				{
+					title: 'Good Ending',
+					artist: 'SHUMANGA',
+					description: 'Рука об руку. Вопреки всем преградам. Она идет и улыбается Ей. Все ещё печальные, потерянные и сломленные, они шли вдоль тропинки к месту, где впервые встретились. Не зная, что будет завтра, девушки были счастливы здесь и сейчас.'
+				}
 			],
 			links: {
 				embed: 'https://bandcamp.com/EmbeddedPlayer/album=3977908732/size=large/bgcol=ffffff/linkcol=0687f5/artwork=none/transparent=true',
